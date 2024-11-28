e01bcfa2b0c54821bc11a941a2ff044bf011f6f9
Arvind Ramaswami <saramaswami@gmail.com>
2024-11-28 12:06:12 -0800

    add empty file. todo: write the logic in it

---
+++ check_equivalence.py
@@ -0,0 +1,5 @@
+# Things that this file should do:
+
+# - A function to check if two equations are equivalent
+# - A parser to parse text and convert it into an equation (could use some python smt parsing library, combined with some llm assistance)
+# - Parse the json file, and see how many of the things are valid
