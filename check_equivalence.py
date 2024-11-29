29dd9589940c35b0a447cb9c22f58c7bb7b4a4bc
Arvind Ramaswami <saramaswami@gmail.com>
2024-11-28 21:27:39 -0800

    add some preliminary stuff

--- check_equivalence.py
+++ check_equivalence.py
@@ -1,5 +1,24 @@
+from z3 import *
 # Things that this file should do:
 
 # - A function to check if two equations are equivalent
 # - A parser to parse text and convert it into an equation (could use some python smt parsing library, combined with some llm assistance)
 # - Parse the json file, and see how many of the things are valid
+
+def convert_to_z3(parsed):
+
+    pass
+
+
+def check_equivalence(equation1, equation2):
+    equation1 = convert_to_z3(equation1)
+    equation2 = convert_to_z3(equation2)
+    # use z3 to check if the two equations are equivalent below
+    s = Solver()
+    s.add(equation1 != equation2)
+    if s.check() == unsat:
+        return True
+    else:
+        return False
+
+
